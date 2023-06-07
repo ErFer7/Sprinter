dbe4925c675903834dae6ddb5df9b2b4b6d390ca
Eric Fernandes Evaristo <ericfernandes7@gmail.com>
2023-06-07 00:01:17 -0300

    Initial commit

---
+++ backend/src/main/java/br/ufsc/bridge/sprinter/App.java
@@ -0,0 +1,10 @@
+package br.ufsc.bridge.sprinter;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Goodbye World!");
+    }
+}
